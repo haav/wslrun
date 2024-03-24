6b09a3e464cd13b0119cd7ab285b90942efbf737
haav <allar.haav@gmail.com>
2024-03-24 16:59:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# WSLRun
+Allows to run bash scripts directly from windows.
+It's a minimalistic wrapper around "wsl.exe bash" call with argument file path conversion via wslpath.
+Usage example:
+* Set wslrun.exe as default program to open ".sh" files in Windows
+* Double-click on any file with .sh extension is opened automatically in WSL bash.
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Missing file path" << std::endl;
+        return 1;
+    }
+    try {
+        std::string filePath = argv[1];
+        char buffer[256];
+        std::string wslCommand = "C:\\Windows\\System32\\wsl.exe bash -c \"$(wslpath '" + filePath + "')";
+        for (int i = 2; i < argc; i++){
+            std::string arg = argv[i];
+            wslCommand += " " + arg;
+        }
+        wslCommand += "\"";
+        FILE* wslProcess = _popen(wslCommand.c_str(), "r");
+        if (!wslProcess) {
+            std::cerr << "Error running WSL." << std::endl;
+            return 1;
+        }
+        while (fgets(buffer, sizeof(buffer), wslProcess)) {
+            std::cout << buffer;
+        }
+        std::cout << std::endl;
+        _pclose(wslProcess);
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return 1;
+    }
+}
